be044a5759a7de26f93d3706bc9c5a53ae93adc1
Reina Uhler <reinadu@gmail.comgit>
2020-12-09 13:33:57 -0800

    test

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
+const express = require("express");
 const mysql = require("mysql");
+const selectUser = require("./queries/selectUser");
+const { toJson, toSafeParse } = require("./utils/helpers");
+
 const connection = mysql.createConnection({
    host: process.env.RDS_HOST,
    user: process.env.RDS_USER,
@@ -9,11 +13,18 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query("SELECT 1 + 1 AS solution", (error, results, fields) => {
-   if (error) {
-      throw error;
+connection.query(selectUser("reinadu@gmail.com", "replace_me"), (err, res) => {
+   if (err) {
+      console.log(err);
    } else {
-      console.log("The solution is: ", results[0].solution);
+      // const user = toSafeParse(toJson(res))[0];
+      const jsonRes = toJson(res);
+      console.log(jsonRes);
+      const parsedRes = toSafeParse(jsonRes);
+      console.log(parsedRes);
+      const firstObj = parsedRes[0];
+      const user = firstObj;
+      console.log(user);
    }
 });
 
---
+++ utils/helpers.js
@@ -0,0 +1,14 @@
+module.exports = {
+   toJson(data) {
+      return JSON.stringify(data);
+   },
+   toSafeParse(str) {
+      try {
+         JSON.parse(str);
+      } catch (err) {
+         console.log(err);
+         return str;
+      }
+      return JSON.parse(str);
+   },
+};
