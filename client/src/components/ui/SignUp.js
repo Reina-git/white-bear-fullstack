dee6296c064f991ede24eceb1d112ba86fe36502
Reina Uhler <reinadu@gmail.comgit>
2020-12-14 17:20:22 -0800

    fixed bug on server.js

--- client/src/components/ui/SignUp.js
+++ client/src/components/ui/SignUp.js
@@ -166,7 +166,6 @@ class SignUp extends React.Component {
                         <div className="input-group mb-3 mt-2">
                            <input
                               type="text"
-                              className="form-control"
                               id="sign-up-password-input"
                               aria-label="Sizing example input"
                               aria-describedby="inputGroup-sizing-default"
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 app.use("/api/v1/memory-cards", require("./api/v1/memory-cards"));
 app.use("/api/v1/users", require("./api/v1/users"));
 
-app, use(express.static("client/build"));
+app.use(express.static("client/build"));
 app.get("*", (req, res) => {
    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
